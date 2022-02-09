dc033136406524d7ae7f562b0e5af4e853a6a3cf
leesuun <77526673+leesuun@users.noreply.github.com>
2022-02-09 22:38:49 +0900

    react set

---
+++ webgame/lecture/WordRelay.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class WordRelay extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+
+module.exports = WordRelay
---
+++ webgame/lecture/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import WordRelay from "./WordRelay"
+
+
+ReactDom.render(<WordRelay/>,document.querySelector("#root"))
