8d07205f8171f4846ee31f14ce705e9351685624
leesuun <77526673+leesuun@users.noreply.github.com>
2022-02-15 23:37:48 +0900

    add App

--- react-basic/react-tutorial/src/index.js
+++ react-basic/react-tutorial/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <div>
+        <App />
+        <App />
+    </div>,
+    document.querySelector("#root")
+);
